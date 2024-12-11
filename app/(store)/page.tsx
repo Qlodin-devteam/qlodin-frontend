161c18957fa362f6319cea3b8a02c81eef7f6a66
TeD <theooye7@gmail.com>
2024-12-11 15:15:44 +0100

    modified:   app/(store)/page.tsx 	modified:   package-lock.json

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Qlodin</h1>
-      <Button>Click here </Button>
+    <div className="flex min-h-screen items-center justify-center bg-gray-200">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold text-gray-800">Welcome to Qlodin</h2>
+        <p className="my-2 text-gray-600">Please follow through to enjoy our services.</p>
+        <Link href="/signuppage"> <Button>Sign up</Button></Link> 
+      </div>
     </div>
-   
   );
 }
